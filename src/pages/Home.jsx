016fe84330f4cf1410cd3635168262545335fe44
PedroPassos87 <peagacarreira@gmail.com>
2025-04-08 00:39:42 -0300

    first commit vite react

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(){
+    return (
+        <div>
+            <h1>Elo Drinks lalala</h1>
+            <p>Bar venda pacote personaliza</p>
+        </div>
+    )
+}
+
+export default Home
