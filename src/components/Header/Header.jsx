3da310c763d250da09f0a19b595733a9a85d6759
PedroPassos87 <peagacarreira@gmail.com>
2025-04-10 01:49:39 -0300

    teste: criação de uma header basica pra home #8

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <span>ELO</span>
+        <span>DRINKS</span>
+      </div>
+
+      <nav className="nav">
+        <ul>
+          <li><a href="#quem-somos">QUEM SOMOS</a></li>
+          <li><a href="#pacotes">PACOTES</a></li>
+          <li><a href="#personalizar">PERSONALIZAR</a></li>
+          <li><a href="#como-funciona">COMO FUNCIONA</a></li>
+        </ul>
+      </nav>
+
+      <div className="login-icon">
+        <i className="fa-regular fa-user"></i>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
