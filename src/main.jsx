42e412803fe573b3067c748b82cf3775ce1df67a
PedroPassos87 <peagacarreira@gmail.com>
2025-04-10 01:50:29 -0300

    pckjson

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRoutes from "./Routes";
-import "./index.css";
+import AppRoutes from "./Routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
