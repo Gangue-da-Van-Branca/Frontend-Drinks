a9240238456c0b0cdc13ae44a2fa23b99651a8d4
PedroPassos87 <peagacarreira@gmail.com>
2025-04-14 15:50:56 -0300

    Base da seção de funcionamento

---
+++ src/components/ComoFunciona/ComoFunciona.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./ComoFunciona.css";
+
+const QuemSomos = () => {
+  return (
+    <section className="como-funciona" id="como-funciona">
+      <div className="titleFunciona">
+        <h1>COMO</h1>
+        <p>ELO DRINKS</p>
+      </div>
+      <div className="teste">
+        <div className="box-esq"></div>
+        <p>FUNCIONAMOS</p>
+        <div className="line-top"></div>
+      </div>
+
+
+    </section>
+  );
+};
+
+export default QuemSomos;
