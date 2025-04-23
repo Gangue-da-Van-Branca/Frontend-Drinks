7aa97bd550cf5de64194fdd7aaceedc8e13b7472
PedroPassos87 <peagacarreira@gmail.com>
2025-04-23 15:58:29 -0300

    alterando rotas

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Cliente/Home/Home";
-import Personalizar from "../pages/Cliente/Personalizar";
 import Cadastro from "../pages/Cadastro/Cadastro";
 import Dashboard from "../pages/Cliente/Dashboard";
 import OrcamentoResumo from "../pages/Cliente/OrcamentoResumo";
@@ -11,11 +10,10 @@ export default function AppRoutes() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pacotes" element={<Pacotes />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/personalizar" element={<Personalizar />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/orcamento-resumo" element={<OrcamentoResumo />} />
-        <Route path="/pacotes" element={<Pacotes />} />
       </Routes>
     </Router>
   );
