b4eb42be9fd608825d6218bb05e9f72aeb782ca8
PedroPassos87 <peagacarreira@gmail.com>
2025-05-06 00:36:02 -0300

    navegacao do botao personalizar

--- src/components/PacotesPage/BotaoPacotesPersonalizar/BotaoPacotesPersonalizar.jsx
+++ src/components/PacotesPage/BotaoPacotesPersonalizar/BotaoPacotesPersonalizar.jsx
@@ -1,7 +1,14 @@
 import "./BotaoPacotesPersonalizar.css";
+import { useNavigate } from "react-router-dom";
 
 function BotaoPersonalizar() {
-  return <div className="botao-personalizar">PERSONALIZAR MEU PACOTE</div>;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/monte-sua-festa");
+  };
+
+  return <div className="botao-personalizar" onClick={handleClick}>PERSONALIZAR MEU PACOTE</div>;
 }
 
 export default BotaoPersonalizar;
