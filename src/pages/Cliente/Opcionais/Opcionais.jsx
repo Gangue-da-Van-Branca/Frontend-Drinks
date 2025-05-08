65fa084ac78ff68cac349fd46c0c3fbc9c8de567
PedroPassos87 <peagacarreira@gmail.com>
2025-05-08 00:30:03 -0300

    botao para a proxima fase da compra

--- src/pages/Cliente/Opcionais/Opcionais.jsx
+++ src/pages/Cliente/Opcionais/Opcionais.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import TopoOpcionais from "../../../components/OpcionaisPage/TopoOpcionais/TopoOpcionais";
 import BaresOpcionais from "../../../components/OpcionaisPage/BaresOpcionais/BaresOpcionais";
 import ShotsOpcionais from "../../../components/OpcionaisPage/ShotsOpcionais/ShotsOpcionais";
 import ExtrasOpcionais from "../../../components/OpcionaisPage/ExtrasOpcionais/ExtrasOpcionais";
 import "./Opcionais.css";
 
-function Opcionais(){
-    return(
-        <div>
-            <TopoOpcionais />
-            <BaresOpcionais />
-            <ShotsOpcionais />
-            <ExtrasOpcionais />
-            
-        </div>
-    )
+function Opcionais() {
+  const navigate = useNavigate();
+
+  const handleAvancar = () => {
+    navigate("/infosContratante");
+  };
+  
+  return (
+    <div>
+      <TopoOpcionais />
+      <BaresOpcionais />
+      <ShotsOpcionais />
+      <ExtrasOpcionais />
+      <div className="container-botao">
+        <button id="botao-avancar" onClick={handleAvancar}>
+          Avançar
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Opcionais;
