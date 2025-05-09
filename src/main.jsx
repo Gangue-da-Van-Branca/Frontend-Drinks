adfb8b54093bc9aaf94dcece70fa5a41123652ec
PedroPassos87 <peagacarreira@gmail.com>
2025-05-09 12:25:21 -0300

    alterando main para usar context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./Routes/Routes";
+import { OrcamentoProvider } from "./context/OrcamentoContext";
 import './style/fonts.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppRoutes />
+    <OrcamentoProvider>
+      <AppRoutes />
+    </OrcamentoProvider>
   </React.StrictMode>
 );
