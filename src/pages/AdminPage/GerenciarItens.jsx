fd30e3a7a8d3780289022dc2a457a67921b54f54
PedroPassos87 <peagacarreira@gmail.com>
2025-05-29 20:46:49 -0300

    hotfix form adm

--- src/pages/AdminPage/GerenciarItens.jsx
+++ src/pages/AdminPage/GerenciarItens.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PacoteForm from "../../components/AdminPageComponents/PacoteForm";
 import DrinkForm from "../../components/AdminPageComponents/DrinkForm";
 import BarForm from "../../components/AdminPageComponents/BarForm";
 import OpcionalForm from "../../components/AdminPageComponents/OpcionalForm";
@@ -7,7 +6,6 @@ import OpcionalForm from "../../components/AdminPageComponents/OpcionalForm";
 export default function GerenciarItens() {
   return (
     <div className="gerenciar-container">
-      <PacoteForm />
       <DrinkForm />
       <BarForm />
       <OpcionalForm />
