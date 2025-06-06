1b2f293317a1e4524efa7592ee6bb6b8d8bbef69
Felipe Conrado <fconrado2003@gmail.com>
2025-06-06 18:32:20 -0300

    resumo orcamento

--- src/components/InfosCompra/OrcamentoResumo.jsx
+++ src/components/InfosCompra/OrcamentoResumo.jsx
@@ -38,64 +38,102 @@ export default function OrcamentoResumo() {
         </div>
 
         <img src={logo} alt="logo" id="logo" />
-
-        <div className="infos-form">
-          <div className="section-title">DADOS DO CONTRATANTE</div>
-          <div id="linha-horizontal-infos" />
-          <div id="resumo-form-grid">
-            {renderCampo("Nome", infosContratante.nome)}
-            {renderCampo("Sobrenome", infosContratante.sobrenome)}
-            {renderCampo("Telefone", infosContratante.telefone)}
-            {renderCampo("Email", infosContratante.email)}
-            {renderCampo("Data", infosContratante.data)}
-            {renderCampo("Endereço", infosContratante.endereco)}
-            {renderCampo("CEP", infosContratante.cep)}
-            {renderCampo("Horário Início", infosContratante.horarioInicio)}
-            {renderCampo("Horário Final", infosContratante.horarioFinal)}
-            {renderCampo("Convidados", infosContratante.convidados)}
-          </div>
-
-          <div className="section-title">BASE DA FESTA</div>
-          <div id="linha-horizontal-infos" />
-          <div className="form-grid">
-            {Object.entries(baseFesta).map(([key, value]) => {
-              let valorFormatado;
-
-              if (Array.isArray(value)) {
-                valorFormatado = value.map((v) => v.nome || v).join(", ");
-              } else if (typeof value === "object" && value !== null) {
-                valorFormatado = value.nome || JSON.stringify(value);
-              } else {
-                valorFormatado = value;
-              }
-
-              return renderCampo(key, valorFormatado);
-            })}
-          </div>
-
-          <div className="section-title">OPCIONAIS</div>
-          <div id="linha-horizontal-infos" />
-
-          <div className="form-grid">
-            <div>
-              <strong>Shots:</strong>
-              <ul>
-                {Object.entries(opcionais.shots).map(([key, val]) => (
-                  <li key={key}>
-                    {key}: {val}
-                  </li>
-                ))}
-              </ul>
+        <div id="colunas">
+          <div id="enquadramento">
+            <div className="infos-form">
+              <div className="section-title" id="orc-sec">
+                DADOS DO CONTRATANTE
+              </div>
+              <div id="resumo-form-grid">
+                {renderCampo(
+                  "Nome",
+                  `${infosContratante.nome || ""} ${
+                    infosContratante.sobrenome || ""
+                  }`.trim()
+                )}
+                {renderCampo("Telefone", infosContratante.telefone)}
+                {renderCampo("Email", infosContratante.email)}
+                {renderCampo("Data", infosContratante.data)}
+                {renderCampo("Endereço", infosContratante.endereco)}
+                {renderCampo("CEP", infosContratante.cep)}
+                {renderCampo("Horário Início", infosContratante.horarioInicio)}
+                {renderCampo("Horário Final", infosContratante.horarioFinal)}
+                {renderCampo("Convidados", infosContratante.convidados)}
+              </div>
             </div>
-            <div>
-              <strong>Extras:</strong>
-              <ul>
-                {Object.entries(opcionais.extras).map(([key, val]) => (
-                  <li key={key}>
-                    {key}: {val}
-                  </li>
-                ))}
-              </ul>
+
+            <div className="infos-form">
+              <div className="section-title" id="orc-sec">
+                BASE DA FESTA
+              </div>
+              <div className="form-grid">
+                {Object.entries(baseFesta)
+                  .slice(1)
+                  .map(([key, value]) => {
+                    let valorFormatado;
+
+                    if (Array.isArray(value)) {
+                      valorFormatado = (
+                        <ul>
+                          {value.map((v, index) => (
+                            <li key={index}>{v.nome || v}</li>
+                          ))}
+                        </ul>
+                      );
+                    } else if (typeof value === "object" && value !== null) {
+                      valorFormatado = value.nome || JSON.stringify(value);
+                    } else {
+                      valorFormatado = value;
+                    }
+
+                    return renderCampo(key, valorFormatado);
+                  })}
+              </div>
+            </div>
+          </div>
+
+          <div id="enquadramento">
+            <div className="section-title" id="orc-sec">
+              OPCIONAIS
+            </div>
+
+            <div className="form-grid">
+              {Object.values(opcionais.shots).some((val) => val > 0) && (
+                <div>
+                  <strong>Shots:</strong>
+                  <ul>
+                    {Object.entries(opcionais.shots)
+                      .filter(([_, val]) => val > 0)
+                      .map(([key, val]) => (
+                        <li key={key}>
+                          {key}: {val}
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+            <div className="form-grid">
+              {Object.values(opcionais.extras).some((val) => val > 0) && (
+                <div>
+                  <strong>Extras:</strong>
+                  <ul>
+                    {Object.entries(opcionais.extras)
+                      .filter(([_, val]) => val > 0)
+                      .map(([key, val]) => (
+                        <li key={key}>
+                          {key}: {val}
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div id="enquadramento">
+             <div className="section-title" id="orc-sec">
+              BARES
             </div>
             <div>
               <strong>Bares Adicionais:</strong>
@@ -109,11 +147,6 @@ export default function OrcamentoResumo() {
                 )}
               </ul>
             </div>
-             <div className="form-footer" style={{ marginTop: "40px" }}>
-            <button type="button" onClick={handleConfirmar}>
-              CONFIRMAR ENVIO
-            </button>
-          </div>
           </div>
         </div>
       </div>
---
+++ src/pages/Cliente/InfosCompra/OrcamentoResumo.jsx
@@ -0,0 +1,10 @@
+import OrcamentoResumo from "../../../components/InfosCompra/OrcamentoResumo";
+
+function InfosCompra(){
+    return(
+        <div>
+            <OrcamentoResumo />
+        </div>
+    )
+}
+export default OrcamentoResumo
